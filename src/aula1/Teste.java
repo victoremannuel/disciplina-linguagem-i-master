8032f0373d435580910bbd35f31304d0fd2d6878
victor <vic.emannuel@gmail.com>
2024-02-08 21:17:47 -0300

    first commit

---
+++ src/aula1/Teste.java
@@ -0,0 +1,15 @@
+package aula1;
+
+public class Teste {
+    public static void main(String[] args) {
+        Cachorro c1 = new Cachorro();
+        Cachorro c2 = new Cachorro();
+        c1.correr();
+        c2.raca = "pitbull";
+        System.out.println(c2.raca);
+
+//        c1.emitirSom("au au");
+//        Cachorro.emitirSom("au au au");
+//        Cachorro.correr();
+    }
+}
